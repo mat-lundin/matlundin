81edd93553d9fae501551a359e440aaf75f1d0f9
Mat Lundin <matlundin.dev@gmail.com>
2022-08-30 18:31:09 -0500

    nav options right aligned, updated colors

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,31 +1,31 @@
 import { NavLink } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 
 
 const nav = () => {
   return (
     // replace brand image
-    // left justify nav options
     // make background a gradient from dark on the left to light on the right so you can see the options once they are left-justified
-    <Navbar collapseOnSelect expand="md" style={{  backgroundSize: 'cover', backgroundColor:"gray" }}>
+    <Navbar collapseOnSelect expand="md" style={{ backgroundSize: 'cover', backgroundColor: "gray" }}>
       <Navbar.Brand style={{ margin: 0, padding: 0 }} href="/"><img src="images/header.png" alt="Mat Lundin" width="80%" /></Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" className="custom-toggler" />
       <Navbar.Collapse className="collapse navbar-collapse" id="navbarCollapse">
 
+        <Nav className="justify-content-end" style={{ width: "100%" }}>
+          <NavLink data-toggle="collapse" data-target="#navbarCollapse" className="nav-link" activeclassname="true" to='/dev' style={({ isActive }) => ({
+            color: isActive ? '#FFFFFF' : '#1D253F', fontSize: '2em'
+          })}>Projects&nbsp;</NavLink>
 
-        <NavLink data-toggle="collapse" data-target="#navbarCollapse" className="nav-link" activeclassname="true" to='/dev' style={({ isActive }) => ({
-          color: isActive ? '#1D253F' : '#66FF00', fontSize: '2em'
-        })}>Projects&nbsp;</NavLink>
+          <NavLink data-toggle="collapse" data-target="#navbarCollapse" className="nav-link" activeclassname="true" to='/music' style={({ isActive }) => ({
+            color: isActive ? '#FFFFFF' : '#1D253F', fontSize: '2em'
+          })}>Music&nbsp;</NavLink>
 
-        <NavLink data-toggle="collapse" data-target="#navbarCollapse" className="nav-link" activeclassname="true" to='/music' style={({ isActive }) => ({
-          color: isActive ? '#1D253F' : '#66FF00', fontSize: '2em'
-        })}>Music&nbsp;</NavLink>
-
-        <NavLink data-toggle="collapse" data-target="#navbarCollapse" className="nav-link" activeclassname="true" to='/resume' style={({ isActive }) => ({
-          color: isActive ? '#1D253F' : '#66FF00', fontSize: '2em'
-        })}>Resume&nbsp;</NavLink>
-
+          <NavLink data-toggle="collapse" data-target="#navbarCollapse" className="nav-link" activeclassname="true" to='/resume' style={({ isActive }) => ({
+            color: isActive ? '#FFFFFF' : '#1D253F', fontSize: '2em'
+          })}>Resume&nbsp;</NavLink>
+        </Nav>
 
       </Navbar.Collapse>
 
