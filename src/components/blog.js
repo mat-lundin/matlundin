021489950d250de567e8022ecade89d14ca4ae67
Mat Lundin <matlundin.dev@gmail.com>
2022-08-30 23:31:42 -0500

    switched markdown package, got a blog post to load as html

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,30 +1,57 @@
 // try accordion with react-markdown https://blog.logrocket.com/how-to-safely-render-markdown-using-react-markdown/
 // turn github gists into blog posts to start
 import Accordion from 'react-bootstrap/Accordion';
-import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
+// import Image from 'react-bootstrap/Image';
+// import Button from 'react-bootstrap/Button';
+import blogpost from '../blogs/regex_tutorial.md';
+import Markdown from 'markdown-to-jsx';
+import React, { useState, useEffect } from 'react';
 
+// https://dev.to/anobjectisa/how-to-dynamically-load-markdown-files-in-react-markdown-to-jsx-53fl
+// https://www.npmjs.com/package/markdown-to-jsx
+const Blog = () => {
+    const [post, setPost] = useState('')
 
-const blog = () => {
+    useEffect(()=>{
+        import('../blogs/regex_tutorial.md')
+        .then(res => {
+            fetch(res.default)
+            .then(res => res.text())
+            .then(res => setPost(res))
+            .catch(err => console.log(err))
+        })
+    })
     return (
         <>
-        <h2>As I learn and grow as a developer, I'll write my thoughts here:</h2>
-        </>
+        <div>
+            <Markdown>
+                {post}
+            </Markdown>
+        </div>
 
-        // <Accordion defaultActiveKey="0">
-        //     {projects.map((project) => {
-        //         return (
-        //             <Accordion.Item eventKey={project.name} key={project.name}>
-        //                 <Accordion.Header>{project.name} - {project.description}</Accordion.Header>
-        //                 <Accordion.Body>
-        //                     <Image src={project.image} fluid="1" thumbnail="1" style={{ height: "20%", width: "20%" }}></Image>
-        //                     <p><Button>Visit</Button>
-        //                     <Button>Github</Button></p>
-        //                 </Accordion.Body>
-        //             </Accordion.Item>)
-        //     })}
-        // </Accordion>
+        {/* <h2>As I learn and grow as a developer, I'll write my thoughts here:</h2>
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+        <ReactMarkdown>{postStr}</ReactMarkdown>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion> */}
+        </>
     )
 }
 
-export default blog
+export default Blog
