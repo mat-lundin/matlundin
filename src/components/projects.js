2c9f61b35959b61761940935ad0ac48e340f78b3
Mat Lundin <matlundin.dev@gmail.com>
2022-09-16 16:23:01 -0500

    added simple tooltip to images

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,35 +3,49 @@ import Table from 'react-bootstrap/Table';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 import projectData from '../projects.json';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
 
 // have a modal or a drawer with the screenshot and links instead of accordion
 // once you open the project the pop up will have an X to quit, link buttons, and you can use the arrow keys to go between projects in the carousel
 // replace accordion with table of name and image on left, description in middle, links on right
 const dev = () => {
+
     return (
-        <div style={{margin: '6%'}}>
-        <h2 style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>Coding Projects I've Worked On</h2>
-        <Table striped responsive>
-      <thead>
-        <tr>
-          <th>img</th>
-          <th>name</th>
-          <th>desc</th>
-          <th>links</th>
-        </tr>
-      </thead>
-      <tbody>
-        {projectData.map((project)=>{
-            return(
-                <tr key={project.name}>
-                    <td><Image src={project.image} fluid  style={{ height: "20%", width: "20%" }} alt={project.name}></Image></td>
-                    <td>{project.name}</td>
-                    <td>{project.description}</td>
-                    <td><Button>Visit</Button><Button>Github</Button></td>
-                </tr>
-            )
-        })}
-        {/* <tr>
+        <div style={{ margin: '6%' }}>
+            <h2 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>Coding Projects I've Worked On</h2>
+            <Table striped responsive>
+                <thead>
+                    <tr>
+                        <th width="11%">img</th>
+                        <th>name</th>
+                        <th>desc</th>
+                        <th width="11%">links</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {projectData.map((project) => {
+                        const renderTooltip = (props) => (
+                            <Tooltip id="button-tooltip" {...props}>
+                                {project.name}
+                            </Tooltip>)
+                        return (
+                            <tr key={project.name}>
+                                <td>    <OverlayTrigger
+                                    placement="right"
+                                    delay={{ show: 250, hide: 400 }}
+                                    overlay={renderTooltip}
+                                >
+                                    <Image src={project.image} fluid style={{ margin: 0 }} alt={project.name}></Image>
+                                </OverlayTrigger></td>
+                                <td>{project.name}</td>
+                                <td>{project.description}</td>
+                                <td><Button style={{ margin: "1%" }}>Visit</Button><Button>Github</Button></td>
+                            </tr>
+                        )
+                    })}
+                    {/* <tr>
           <td>1</td>
           <td>Mark</td>
           <td>Otto</td>
@@ -48,8 +62,8 @@ const dev = () => {
           <td colSpan={2}>Larry the Bird</td>
           <td>@twitter</td>
         </tr> */}
-      </tbody>
-    </Table>
+                </tbody>
+            </Table>
         </div>
     )
 }
