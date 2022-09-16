b2b1db922027d624edcec15a173652d2f8f1c625
Mat Lundin <matlundin.dev@gmail.com>
2022-09-16 15:43:15 -0500

    initialized project table

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,8 +1,8 @@
-// try accordion and if it looks bad use cards again
-import Accordion from 'react-bootstrap/Accordion';
+// table for projects instead of cards or accordion
+import Table from 'react-bootstrap/Table';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
-import projects from '../projects.json';
+import projectData from '../projects.json';
 
 // have a modal or a drawer with the screenshot and links instead of accordion
 // once you open the project the pop up will have an X to quit, link buttons, and you can use the arrow keys to go between projects in the carousel
@@ -11,20 +11,45 @@ const dev = () => {
     return (
         <div style={{margin: '6%'}}>
         <h2 style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>Coding Projects I've Worked On</h2>
-        <Accordion defaultActiveKey="0">
-            {projects.map((project) => {
-                return (
-                    <Accordion.Item eventKey={project.name} key={project.name}>
-                        {/* align name and description like Pipe Dreams looks? */}
-                        <Accordion.Header><p><h5>{project.name}</h5></p> <p>{project.description}</p> </Accordion.Header>
-                        <Accordion.Body style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
-                            <Image src={project.image} fluid="1" thumbnail="1" style={{ height: "20%", width: "20%" }} alt={project.name}></Image>
-                            <Button style={{margin: '.6%'} } href={project.url}>Visit</Button>
-                            <Button href={project.repoUrl}>Github</Button>
-                        </Accordion.Body>
-                    </Accordion.Item>)
-            })}
-        </Accordion>
+        <Table striped>
+      <thead>
+        <tr>
+          <th>img</th>
+          <th>name</th>
+          <th>desc</th>
+          <th>links</th>
+        </tr>
+      </thead>
+      <tbody>
+        {projectData.map((project)=>{
+            return(
+                <tr key={project.name}>
+                    {/* <td><Image>{project.image}</Image></td> */}
+                    <td>{project.name}</td>
+                    <td>{project.description}</td>
+                    <td><Button>Visit</Button><Button>Github</Button></td>
+                </tr>
+            )
+        })}
+        {/* <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr> */}
+      </tbody>
+    </Table>
         </div>
     )
 }
