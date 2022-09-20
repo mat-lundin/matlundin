fc3d90782b50324366875442017d5785c9cbbb9e
Mat Lundin <matlundin.dev@gmail.com>
2022-09-20 16:38:43 -0500

    added new post to tech blog

--- src/components/blog.js
+++ src/components/blog.js
@@ -10,45 +10,51 @@ import React, { useState, useEffect } from 'react';
 // https://dev.to/anobjectisa/how-to-dynamically-load-markdown-files-in-react-markdown-to-jsx-53fl
 // https://www.npmjs.com/package/markdown-to-jsx
 const Blog = () => {
-    const [post, setPost] = useState('')
+  const [post0, setPost0] = useState('')
+  const [post1, setPost1] = useState('')
 
-    useEffect(()=>{
-        import('../blogs/regex_tutorial.md')
-        .then(res => {
-            fetch(res.default)
-            .then(res => res.text())
-            .then(res => setPost(res))
-            .catch(err => console.log(err))
-        })
-    })
-    return (
-        <div style={{margin:'6%'}}>
-        <h2 style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>As I learn and grow as a developer, I'll write my thoughts here:</h2>
-{/* to add an already open item, do <Accordion defaultActiveKey="0"> */}
-        <Accordion>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>How Does The Regular Expression To Find an HTML Tag Work?</Accordion.Header>
-        <Accordion.Body>
-        <Markdown>
-                {post}
+  useEffect(() => {
+    import('../blogs/regex_tutorial.md')
+      .then(res => {
+        fetch(res.default)
+          .then(res => res.text())
+          .then(res => setPost0(res))
+          .catch(err => console.log(err))
+      })
+  })
+  useEffect(() => {
+    import('../blogs/qr_codes.md')
+      .then(res => {
+        fetch(res.default)
+          .then(res => res.text())
+          .then(res => setPost1(res))
+          .catch(err => console.log(err))
+      })
+  })
+  return (
+    <div style={{ margin: '6%' }}>
+      <h2 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>As I learn and grow as a developer, I'll write my thoughts here:</h2>
+      {/* to add an already open item, do <Accordion defaultActiveKey="0"> */}
+      <Accordion>
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>How Does The Regular Expression To Find an HTML Tag Work?</Accordion.Header>
+          <Accordion.Body>
+            <Markdown>
+              {post0}
             </Markdown>
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-        </div>
-    )
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>QR Codes</Accordion.Header>
+          <Accordion.Body>
+            <Markdown>
+              {post1}
+            </Markdown>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </div>
+  )
 }
 
 export default Blog
