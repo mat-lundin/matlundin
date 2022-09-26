3df9bc25aef63467e36c517aa1cba508cd4c1257
Mat Lundin <matlundin.dev@gmail.com>
2022-09-26 00:22:33 -0500

    get filenames from folder, first try at mapping to blog

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,6 +1,9 @@
 import Accordion from 'react-bootstrap/Accordion';
 import Markdown from 'markdown-to-jsx';
 import React, { useState, useEffect } from 'react';
+import AccordionItem from 'react-bootstrap/esm/AccordionItem';
+import AccordionHeader from 'react-bootstrap/esm/AccordionHeader';
+const fs = require('fs')
 
 // concept from https://dev.to/anobjectisa/how-to-dynamically-load-markdown-files-in-react-markdown-to-jsx-53fl
 // render blog post .md files for the accordion items
@@ -27,6 +30,30 @@ const Blog = () => {
       })
   })
 
+  // create array of filenames
+const blogFiles = fs.readdir('../blogs/',(err, data)=>{
+  if (err) {
+    console.log(err)
+  }
+  else {
+    console.log(data)
+    blogPostArr(data)
+  }
+});
+
+const blogPostArr = function(files){
+  files.map((file)=>{
+      import(file)
+        .then(res => {
+          fetch(res.default)
+            .then(res => res.text())
+            .then(res => {return res})
+            .catch(err => console.log(err))
+        })
+    })
+  }
+
+
   return (
     <div style={{ background: 'linear-gradient(to bottom, #808080 0%, #FFFFFF 100%)' }}>
       <h2 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: "2%" }}>My Thoughts About Tech and Development</h2>
@@ -49,8 +76,17 @@ const Blog = () => {
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
+      <Accordion style={{ margin: '20%', marginTop: '1%', marginBottom: '1%', minHeight: '58vh' }}>
+      {blogPostArr.map((post)=>{
+        <AccordionItem eventKey={post}>
+          <AccordionHeader>posties</AccordionHeader>
+          <Accordion.Body>
+            <Markdown>{post}</Markdown>
+          </Accordion.Body>
+        </AccordionItem>
+      })}
+      </Accordion>
     </div>
   )
-}
-
+    }
 export default Blog
